1b560fcc8d35e5622ec2d4ac6a6246626dc87544
Virgile <virgilebatto@gmail.com>
2026-03-18 17:45:11 +0100

    again

--- training_exemples/lerobot_humanoid_no_arms/env_cfgs.py
+++ training_exemples/lerobot_humanoid_no_arms/env_cfgs.py
@@ -354,8 +354,8 @@ def lerobot_humanoid_no_arms_rough_env_cfg(play: bool = False) -> ManagerBasedRl
   """Create LeRobot Humanoid rough terrain velocity configuration."""
   cfg = make_velocity_env_cfg()
 
-  cfg.sim.mujoco.ccd_iterations = 700
-  cfg.sim.contact_sensor_maxmatch = 700
+  cfg.sim.mujoco.ccd_iterations = 500
+  cfg.sim.contact_sensor_maxmatch = 500
   cfg.sim.nconmax = 70
 
   cfg.scene.entities = {"robot": get_lerobot_humanoid_no_arms_robot_cfg()}
