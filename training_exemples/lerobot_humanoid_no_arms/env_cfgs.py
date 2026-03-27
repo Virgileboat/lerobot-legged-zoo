80c7806d736c93ddc93ef3dbfcf479e65c2c4c51
Virgile <virgilebatto@gmail.com>
2026-03-27 10:27:43 +0100

    reduce  AR enality

--- training_exemples/lerobot_humanoid_no_arms/env_cfgs.py
+++ training_exemples/lerobot_humanoid_no_arms/env_cfgs.py
@@ -967,8 +967,8 @@ def lerobot_humanoid_no_arms_rough_env_cfg(play: bool = False) -> ManagerBasedRl
         {"step": 10_000 * 24, "weight": -1.0},
         {"step": 15_000 * 24, "weight": -2.0},
         {"step": 20_000 * 24, "weight": -4.0},
-        {"step": 25_000 * 24, "weight": -10.0},
-        {"step": 30_000 * 24, "weight": -15.0},
+        {"step": 25_000 * 24, "weight": -6.0},
+        {"step": 30_000 * 24, "weight": -6.0},
       ],
     },
   )
